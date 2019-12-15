1629b7e774ae52f58f43dbd48d93b56a1524be02
Krizel Minnema <krizelminnema@gmail.com>
2019-12-15 13:47:05 -0800

    adding pseudocode as base to start the project

--- liri.js
+++ liri.js
@@ -1,3 +1,42 @@
+//=============
+// LIRI : BASIC FUNCTIONS ARE TO SEARCH SPOTIFY FOR SONGS, BANDS IN TOWN FOR CONCERTS AND OMDB FOR MOVIES
+//=============
+
+//=============
+// GLOBAL VARIABLES
+//=============
+
 const dotenv = require("dotenv").config();
 const keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+
+//=============
+// SPOTIFY 
+//=============
+
+// Search for song name, preview link of the song, album that the song is from
+
+// If no song, default to "The Sign" by Ace of Base
+
+//=============
+// BANDS IN TOWN
+//=============
+
+// Name of the venue
+// Venue location
+// Date of the Event (using MM/DD/YYYY)
+
+// LINK MOMENT.JS?
+
+//=============
+// OMDB
+//=============
+
+// * Title of the movie.
+// * Year the movie came out.
+// * IMDB Rating of the movie.
+// * Rotten Tomatoes Rating of the movie.
+// * Country where the movie was produced.
+// * Language of the movie.
+// * Plot of the movie.
+// * Actors in the movie.
